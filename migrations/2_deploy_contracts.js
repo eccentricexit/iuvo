b8161ff40d7f4c7543ac82564d0f9ceacf1bd2d5
Matheus Faria <mtsalenc@gmail.com>
2018-07-30 18:32:41 +0000

    fix: Migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,6 +4,7 @@ const IuvoCoreByProxy = artifacts.require('IuvoCore')
 
 module.exports = async (deployer, network, accounts) => {
   const owner = accounts[0]
+  const ratingOracle = accounts[9]
   const doctorA = accounts[1]
   const doctorB = accounts[2]
   const doctorC = accounts[3]
@@ -16,41 +17,62 @@ module.exports = async (deployer, network, accounts) => {
       await iuvoCoreByProxy.initialize()
 
       console.info('proxy ==> ', iuvoCoreByProxy.address)
+      await iuvoCoreByProxy.setRatingOracle(
+        ratingOracle,
+        { from: owner }
+      )
 
       await iuvoCoreByProxy.setDoctor(
         'Dr. Nancy',
-        '4.4',
         'Love taking care of people',
         'QmcSD36n81qTdHWCiHoFt1jiVpcW1eZoHJALFbBJxYRhLf',
         'QmeKSTWokWbyJ8BG122WLty4adXi1mXEee2evxuHQWNfYm',
         { from: doctorA }
       )
+      await iuvoCoreByProxy.setRating(
+        doctorA,
+        "4.2",
+        { from: ratingOracle }
+      )
 
       await iuvoCoreByProxy.setDoctor(
         'Dr. John McClaine',
-        '4.0',
         'Building relationships one happy patient at a time',
         'QmPHFmDDcaXWPu7yxUC3PnZBvo9B61MgxHu6YUTsH7C8hD',
         'QmeKSTWokWbyJ8BG122WLty4adXi1mXEee2evxuHQWNfYm',
         { from: doctorB }
       )
+      await iuvoCoreByProxy.setRating(
+        doctorB,
+        "4.0",
+        { from: ratingOracle }
+      )
 
       await iuvoCoreByProxy.setDoctor(
         'Dr. Naman',
-        '4.1',
         'Ready to make you healthier!',
         'QmYnz2Bsnrt6cvatDgoxwFVNBLknfxnrpduYkxqfYHpuwT',
         'QmeKSTWokWbyJ8BG122WLty4adXi1mXEee2evxuHQWNfYm',
         { from: doctorC }
       )
+      await iuvoCoreByProxy.setRating(
+        doctorC,
+        "4.4",
+        { from: ratingOracle }
+      )
 
       await iuvoCoreByProxy.setDoctor(
         'Dr. Ellen',
-        '4.0',
         'Come in sick, go out treated',
         'QmSmxngeX8YbswMcjsH3xQskq3v6RvJ1mGRsF1K5oiTXxh',
         'QmeKSTWokWbyJ8BG122WLty4adXi1mXEee2evxuHQWNfYm',
         { from: doctorD }
       )
+      await iuvoCoreByProxy.setRating(
+        doctorD,
+        "3.5",
+        { from: ratingOracle }
+      )
+      
     })
 }
