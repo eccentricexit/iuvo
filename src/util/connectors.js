fee365572001d014bd0824d49d6b7609682d75fe
Matheus Faria de Alencar <mtsalenc@gmail.com>
2018-08-06 15:37:00 -0300

    fix: Update dapp version number

--- src/util/connectors.js
+++ src/util/connectors.js
@@ -1,6 +1,6 @@
 import { Connect, SimpleSigner } from 'uport-connect'
 
-export const uport = new Connect('Iuvo 0.0.1', {
+export const uport = new Connect('Iuvo 1.0.0', {
   clientId: '2oosESr6NaTEM6niz8HZz63n5T82Ay1geMN',
   network: 'rinkeby',
   signer: SimpleSigner('21e1068504b1ab86ce16066a74975288cda8a043a8b75eeca534c701a66ac09d')
