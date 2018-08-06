2c1ae500b5f1eacc270ff5e7dbb59ed66bcbf3d5
matheus.alencar <matheus.alencar@iacit.com.br>
2018-08-06 11:12:26 -0300

    feat: Update README.md and migrations

--- README.md
+++ README.md
@@ -1,5 +1,45 @@
-# Iuvo
+# iuvo
 
-## Rinkeby
-- IuvoCore: 0x675f8bb31c7689df60e7c019e0c4fe9d0d8052f2
-- PausableProxy: 0x5f17193cda988ddecb1c2255c1c4e8077ecc8b66
+<center><img alt="banner" src="assets/iuvo.jpg" /></center>
+
+
+[![standard-readme compliant](https://img.shields.io/badge/standard--readme-OK-green.svg?style=flat-square)](https://github.com/RichardLitt/standard-readme)
+TODO: Put more badges here.
+
+> The decentralized health services marketplace.
+
+Contracts on Rinkeby:
+- IuvoCore: [TODO](https://rinkeby.etherscan.io/address/TODO)
+- PausableProxy: [TODO](https://rinkeby.etherscan.io/address/TODO)
+
+## Table of Contents
+
+- [iuvo](#iuvo)
+    - [Table of Contents](#table-of-contents)
+    - [Security](#security)
+    - [Install](#install)
+    - [Tests](#tests)
+    - [Usage](#usage)
+    - [License](#license)
+
+## Security
+
+## Install
+
+Simply run `npm install`
+
+## Tests
+
+Run the tests with `truffle test`
+
+This project was built using the [Truffle Framework](), with tests written with [mocha]() and [chai](). They can be found inside the `test` folder.
+
+## Usage
+
+This dapp integrates with uPort. This means you don't need Metamask, but you a smartphone with uPort installed.
+
+Simply run `npm start`.
+
+## License
+
+MIT © 2018 Matheus Alencar
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,12 +4,7 @@ const IuvoCoreByProxy = artifacts.require('IuvoCore')
 
 module.exports = async (deployer, network, accounts) => {
   const owner = accounts[0]
-  const ratingOracle = '0x7556f79d01bd9e56b5fb39804a4de4eb740aa7ee'
-  const doctorA = '0xdd80f75f2a8fc4771c53e99bf9f5f5a5bcb0cc73'
-  const doctorB = '0x0dbb42e12a14e8360c02e35e013be8118a769185'
-  const doctorC = '0x6d066c17d3c2760e9eb2eaa94e025799e7cb6477'
-  const doctorD = '0x40886f09442ae7a5cd2ea8db3a458d41104bf8e2'
-  const patientA = '0x3e6d4b2f753d37ec4d299607ef7a65cb4e2c93d1'
+  const ratingOracle = accounts[1]
 
   deployer.deploy(IuvoCore, { gas: 4612388, from: owner }).then(async () => {
     await deployer.deploy(PausableProxy, IuvoCore.address, { gas: 4612388, from: owner })
@@ -22,70 +17,5 @@ module.exports = async (deployer, network, accounts) => {
       ratingOracle,
       { from: owner, gas: 4612388 }
     )
-
-    // console.info('setting doctor')
-    // await iuvoCoreByProxy.setDoctor(
-    //   'Dr. Nancy',
-    //   'Love taking care of people',
-    //   'QmcSD36n81qTdHWCiHoFt1jiVpcW1eZoHJALFbBJxYRhLf',
-    //   'QmeKSTWokWbyJ8BG122WLty4adXi1mXEee2evxuHQWNfYm',
-    //   { from: doctorA, gas: 4612388 }
-    // )
-
-    // console.info('setting rating')
-    // await iuvoCoreByProxy.setRating(
-    //   doctorA,
-    //   '4.2',
-    //   { from: ratingOracle, gas: 4612388 }
-    // )
-
-    // await iuvoCoreByProxy.setDoctor(
-    //   'Dr. John McClaine',
-    //   'Building relationships one happy patient at a time',
-    //   'QmPHFmDDcaXWPu7yxUC3PnZBvo9B61MgxHu6YUTsH7C8hD',
-    //   'QmeKSTWokWbyJ8BG122WLty4adXi1mXEee2evxuHQWNfYm',
-    //   { from: doctorB, gas: 4612388 }
-    // )
-    // await iuvoCoreByProxy.setRating(
-    //   doctorB,
-    //   '4.0',
-    //   { from: ratingOracle, gas: 4612388 }
-    // )
-
-    // await iuvoCoreByProxy.setDoctor(
-    //   'Dr. Naman',
-    //   'Ready to make you healthier!',
-    //   'QmYnz2Bsnrt6cvatDgoxwFVNBLknfxnrpduYkxqfYHpuwT',
-    //   'QmeKSTWokWbyJ8BG122WLty4adXi1mXEee2evxuHQWNfYm',
-    //   { from: doctorC, gas: 4612388 }
-    // )
-    // await iuvoCoreByProxy.setRating(
-    //   doctorC,
-    //   '4.4',
-    //   { from: ratingOracle, gas: 4612388 }
-    // )
-
-    // await iuvoCoreByProxy.setDoctor(
-    //   'Dr. Ellen',
-    //   'Come in sick, go out treated',
-    //   'QmSmxngeX8YbswMcjsH3xQskq3v6RvJ1mGRsF1K5oiTXxh',
-    //   'QmeKSTWokWbyJ8BG122WLty4adXi1mXEee2evxuHQWNfYm',
-    //   { from: doctorD,gas: 4612388 }
-    // )
-    // await iuvoCoreByProxy.setRating(
-    //   doctorD,
-    //   '3.5',
-    //   { from: ratingOracle, gas: 4612388 }
-    // )
-
-    // await iuvoCoreByProxy.hireDoctor(
-    //   doctorA,
-    //   'QmeKSTWokWbyJ8BG122WLty4adXi1mXEee2evxuHQWNfYm',
-    //   0x0,
-    //   'https://kleros.io',
-    //   100,
-    //   0x0,
-    //   { from: patientA, gas: 4612388 }
-    // )
   })
 }
